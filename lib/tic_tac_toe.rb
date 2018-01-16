5e0d758228175b77dc8de64be6c93ea2cf863ecb
Katrina Brinson <kbrinso@gmail.com>
2018-01-16 17:14:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,9 +42,9 @@ class TicTacToe
     end
   end
   
-  def valid_move?(board,index)
+  def valid_move?(index)
     if index.between?(0,8)
-      if position_taken?(board,index)
+      if position_taken?(index)
         return false
       else 
         return true
