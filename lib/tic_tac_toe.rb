77c1816c9efaa7fe787dc2960f2e30b3d1af74b8
Katrina Brinson <kbrinso@gmail.com>
2018-01-16 17:13:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,5 +31,15 @@ class TicTacToe
     @board[index] = character
     return @board
   end
+  
+  def position_taken?(board,index)
+    if board.length == 0
+      return
+    elsif (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false
+    else
+      return true
+    end
+  end
 
 end
